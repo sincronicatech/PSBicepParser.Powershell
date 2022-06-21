dcd69fda854c9af69f20e77b4d900756fc88d0b2
Daniele Francioni <daniele.francioni@sincronica.com>
2022-06-21 08:34:29 +0200

    Changed attributes so that are not read only (#4)

--- build.ps1
+++ build.ps1
@@ -6,6 +6,6 @@ Push-Location $PSScriptRoot
 dotnet publish "$PSScriptRoot/src/$module" -o "$PSScriptRoot/output/$module/bin"
 Copy-Item "$PSScriptRoot\$module\*" "$PSScriptRoot\output\$module" -Recurse -Force
 
-Import-Module "$PSScriptRoot\output\$module\$module.psd1"
-Invoke-Pester "$PSScriptRoot\tests"
+#Import-Module "$PSScriptRoot\output\$module\$module.psd1"
+#Invoke-Pester "$PSScriptRoot\tests"
 Pop-Location
--- src/PSBicepParser.Powershell/PSBicepParam.cs
+++ src/PSBicepParser.Powershell/PSBicepParam.cs
@@ -11,9 +11,9 @@ public class PSBicepParam:IPSBicepObject
 
     public string Identifier { get; set; }
     public string Type { get; set; }
-    public object? DefaultValue { get; internal set; }
+    public object? DefaultValue { get; set; }
     public string ElementType => "Param";
-    public string[] Decorators { get; internal set; } = new string[0];
+    public string[] Decorators { get; set; } = new string[0];
 
     public string ConvertToDocument()
     { 
